7afb36d8ca035eba5832292293bf6729ca6b374b
Owen Ford <owenford@rogers.com>
2021-01-12 21:10:21 -0500

    footer

--- minotaur-site/src/App.js
+++ minotaur-site/src/App.js
@@ -4,7 +4,13 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import Nav from 'react-bootstrap/Nav';
+import {ReactComponent as ReactLogo} from './logo.svg';
 import './App.css';
+
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
   
@@ -36,8 +42,11 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
+
           <Navbar className="border-bottom" bg="transparent" expand="sm">
-            <Navbar.Brand>Minotaur</Navbar.Brand>
+            <Navbar.Brand>
+              Minotaur
+            </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
@@ -45,9 +54,17 @@ class App extends React.Component {
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
+
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/> }/>
+          <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} /> }/>
+          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} /> }/>
+          <Footer />
+
+
         </Container>
       </Router>
     );
